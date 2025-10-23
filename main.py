d2a61996a86b6c1bd6344284f1c381269eeabcf5
Marco-Chagas <marketingccbeugoiania@gmail.com>
2025-10-23 18:07:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from kerykeion import KrInstance
+from kerykeion import Kerykeion
 
 app = FastAPI()
 
-# Permite que o Lovable ou seu app web acessem esta API
+# Permite que o Lovable ou seu app web acessem este backend
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -15,7 +15,18 @@ app.add_middleware(
 
 @app.get("/natal")
 def get_natal_chart(name: str, year: int, month: int, day: int, hour: int, minute: int, lat: float, lon: float, tz: float):
-    k = KrInstance(name, year, month, day, hour, minute, lat, lon, tz)
+    k = Kerykeion(
+        name=name,
+        year=year,
+        month=month,
+        day=day,
+        hour=hour,
+        minute=minute,
+        lat=lat,
+        lon=lon,
+        tz=tz
+    )
+
     return {
         "sun": k.sun.sign,
         "moon": k.moon.sign,
